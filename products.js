f91567f4120db813080316f7322fc2d165a1686d
eha564 <sonerandel1@gmail.com>
2026-09-18 16:32:37 +0100

    Fix product details and formatting in products.js

--- products.js
+++ products.js
@@ -1,5 +1,6 @@
 const products = [
-  { name: "G45 Gen", price: "$24.99", img: "image/G45 Gen5.jpg", desc: "200g organic pure leaf powder" }
+  { name: "G45 Gen", price: "$340", img: "image/G45 Gen5.jpg", desc: "G45 Gen 5, 9mm. Clean factory setup, no extra sauce just that classic Glock look" },
+  { name: "G17 Gen3, 9mm", price: "$400", image/G17 Gen3, 9mm.jpg", desc: "G17 Gen 3, 9mm. Full-size Glock with the long slide, 17-round mag setup, clean black frame and no extra flashy shit on it" } 
 ];
 // Allows index.html or other scripts to use the product data
 if (typeof module !== 'undefined' && module.exports) {
