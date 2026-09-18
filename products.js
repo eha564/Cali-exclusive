fcbb90c7e01f01c8869296c1e2e5483a5151d603
eha564 <sonerandel1@gmail.com>
2026-09-18 16:47:40 +0100

    Update product prices in products.js

--- products.js
+++ products.js
@@ -1,13 +1,13 @@
 const products = [
   {
     name: "G45 Gen5",
-    price: "$24.99",
+    price: "$340",
     img: "image/G45 Gen5.jpg",
     desc: "G45 Gen 5, 9mm. Clean factory setup, no extra sauce just that classic Glock look."
   },
   {
     name: "G17 Gen3, 9mm",
-    price: "$24.99",
+    price: "$400",
     img: "image/G17 Gen3, 9mm.jpg",
     desc: "G17 Gen 3, 9mm. Full-size Glock with the long slide, 17-round mag setup, clean black frame and no extra flashy shit on it"
   }
